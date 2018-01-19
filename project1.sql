c340843417707ba9652afdc783f72f601720671e
root <root@localhost.localdomain>
2018-01-19 10:14:28 +0700

    INI PROJEK PKL ANJAS YG KE 1

--- project1.sql
+++ project1.sql
@@ -1,23 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 08, 2017 at 05:37 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Jan 19, 2018 at 04:13 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pkl`
+-- Database: `project1`
 --
 
 -- --------------------------------------------------------
@@ -26,12 +28,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-`id` int(11) NOT NULL,
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
   `nama` varchar(256) NOT NULL,
   `username` varchar(256) NOT NULL,
   `password` varchar(245) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
@@ -46,12 +48,12 @@ INSERT INTO `admin` (`id`, `nama`, `username`, `password`) VALUES
 -- Table structure for table `contact`
 --
 
-CREATE TABLE IF NOT EXISTS `contact` (
-`id` int(11) NOT NULL,
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
   `nama` varchar(256) NOT NULL,
   `no_telepon` varchar(15) NOT NULL,
   `no_wa` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `contact`
@@ -66,12 +68,12 @@ INSERT INTO `contact` (`id`, `nama`, `no_telepon`, `no_wa`) VALUES
 -- Table structure for table `komentar`
 --
 
-CREATE TABLE IF NOT EXISTS `komentar` (
-`id` int(11) NOT NULL,
+CREATE TABLE `komentar` (
+  `id` int(11) NOT NULL,
   `nama` varchar(20) NOT NULL,
   `email` varchar(40) NOT NULL,
   `komentar` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `komentar`
@@ -87,23 +89,19 @@ INSERT INTO `komentar` (`id`, `nama`, `email`, `komentar`) VALUES
 -- Table structure for table `list`
 --
 
-CREATE TABLE IF NOT EXISTS `list` (
-`id` int(11) NOT NULL,
+CREATE TABLE `list` (
+  `id` int(11) NOT NULL,
   `foto` varchar(256) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `keterangan` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=243 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `list`
 --
 
 INSERT INTO `list` (`id`, `foto`, `nama`, `keterangan`) VALUES
-(12, '07102017144211DSC_0012.JPG', 'assad', 'sadasd asdsad asdsad sa asd asd sad sad sad '),
-(123, '07102017172246DSC_0009.JPG', 'service ltrouble listrik', 'Ini Adalah salah satu jasa kami'),
-(235, '07102017133441DSC_0009.JPG', 'Anjas Wicaksana Ganteng', 'Asdasdasdsad'),
-(241, '07102017172425DSC_0021.JPG', 'service fire alarm system', 'Salah satu jasa paling prioritas bagi kami'),
-(242, '07102017172459DSC_0007.JPG', 'huydrant', 'ini adalah pompa hydrant untuk mengatasi masalah');
+(243, '23102017091230foto_about.jpg', 'Service Fire Alarm', 'Jasa memasang alarm jika ada kebakaran di perusahaan.');
 
 --
 -- Indexes for dumped tables
@@ -113,25 +111,25 @@ INSERT INTO `list` (`id`, `foto`, `nama`, `keterangan`) VALUES
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `contact`
 --
 ALTER TABLE `contact`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `komentar`
 --
 ALTER TABLE `komentar`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `list`
 --
 ALTER TABLE `list`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -141,22 +139,27 @@ ALTER TABLE `list`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `komentar`
 --
 ALTER TABLE `komentar`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `list`
 --
 ALTER TABLE `list`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=243;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=244;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
